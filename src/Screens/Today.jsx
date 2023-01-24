98da1c49671ca5e2e1ea6f91b37f4aa991e1a28a
hairyElephant <josecalderon353@gmail.com>
2023-01-24 13:52:39 -0500

    something

--- src/Screens/Today.jsx
+++ src/Screens/Today.jsx
@@ -5,25 +5,24 @@ import { useState, useEffect } from "react";
 export default function Today() {
     const [todaysMeals, setTodaysMeals] = useState([]);
     const [toggle, setToggle] = useState(false);
-}
 
     const display = () => {
         if (!toggle) return setToggle(true);
         return setToggle(false)
     }
-
+    
     // useEffect(() => {
-    //     fetchTodaysMeals();
+    //     fetchTodaysMeals(), []
     // })
-
+    
     return (
         <>
         <NavBar />
-
+    
         <h1>Today</h1>
-
+    
         <button onClick={() => display()}>New Log Entry</button>
-
+    
             {(toggle && (
                 <InputModal display={display}/>
             ))}
@@ -32,3 +31,4 @@ export default function Today() {
         </div>
         </>
     )
+}
