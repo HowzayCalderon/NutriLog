7f1405ed1de03568c473bf6655bca008a06755c2
sdmantha <sdangelcake@gmail.com>
2023-01-24 14:37:52 -0500

    made the homepage basic

---
+++ src/Screens/Home.jsx
@@ -0,0 +1,13 @@
+import "../Screens/Home.css";
+import React from "react";
+
+function Home() {
+
+  return (
+    <div className="Home">
+     <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
