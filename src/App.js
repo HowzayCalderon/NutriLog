86105cff59e25198b57ec9844948d08d8268e431
hairyElephant <josecalderon353@gmail.com>
2023-01-24 14:36:55 -0500

    added imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React from 'react'
 import { Route, Routes } from "react-router-dom"
+import Home from './Screens/Home.jsx';
+import History from './Screens/History.jsx'
+import Today from './Screens/Today.jsx'
 
 
 function App() {
