b5bac54d361db5b0f1ff2e8994176c979d374efb
hairyElephant <josecalderon353@gmail.com>
2023-01-24 14:13:16 -0500

    added routes again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
 
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/history' element={<History />} />
+        <Route path='/today' element={<Today />} />
+      </Routes>
     </div>
   );
 }
