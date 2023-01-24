71693c795a4e17aab3d50d10ea4a59393b685033
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-01-23 22:01:03 -0500

    Sketched out Today.jsx w/ toggle for modal

---
+++ src/Screens/Today.jsx
@@ -0,0 +1,34 @@
+import NavBar from '../Components/NavBar.jsx'
+import InputModal from '../Components/InputModal.jsx'
+import { useState, useEffect } from "react";
+
+export default function Today() {
+    const [todaysMeals, setTodaysMeals] = useState([]);
+    const [toggle, setToggle] = useState(false);
+}
+
+    const display = () => {
+        if (!toggle) return setToggle(true);
+        return setToggle(false)
+    }
+
+    useEffect(() => {
+        fetchTodaysMeals();
+    })
+
+    return (
+        <>
+        <NavBar />
+
+        <h1>Today</h1>
+
+        <button onClick={() => display()}>New Log Entry</button>
+
+            {toggle && (
+                <InputModal />
+            )}
+        
+        <div className="todaysEntry">
+        </div>
+        </>
+    )
