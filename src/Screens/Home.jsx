0afad757a6ce175b45db38538f3d079a34f3aab7
hairyElephant <josecalderon353@gmail.com>
2023-01-24 22:32:19 -0500

    Got data to show up

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,11 +1,13 @@
 
 import "../Screens/Home.css";
 import React from "react";
+import NavBar from "../Components/NavBar";
 
 function Home() {
 
   return (
     <div className="Home">
+    <NavBar />
      <h1>Home Page</h1>
     </div>
   );
