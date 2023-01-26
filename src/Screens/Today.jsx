cb499516b5c96cc824d230c7a8f17a87fcfda3fc
hairyElephant <josecalderon353@gmail.com>
2023-01-25 21:33:41 -0500

    added icons

--- src/Screens/Today.jsx
+++ src/Screens/Today.jsx
@@ -3,6 +3,8 @@ import InputModal from "../Components/InputModal.jsx";
 import { useState, useEffect } from "react";
 import { deleteItem, getItems, updateItem } from "../Services/Items.js";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as HiIcons from "react-icons/hi";
 
 export default function Today() {
   const [toggle, setToggle] = useState(false);
@@ -35,7 +37,7 @@ export default function Today() {
 
     // // converts current date + time to milliseconds since Jan 1, 1970
     // const now = date.getTime();
-  
+
     // // wrapper function that will return "today" at 12AM (00:00:00)
     // function resetTime() {
     //   date.setSeconds(0);
@@ -43,18 +45,18 @@ export default function Today() {
     //   date.setHours(0);
     //   return date;
     // }
-  
+
     // resetTime();
-  
+
     // // assigns "today" @ 12AM to variable
     // const thisMorning = date.getTime();
-  
+
     // // adds 1 to the "date" of "today at 12AM", creating "tomorrow" @ 12AM
     // date.setDate(date.getDate() + 1);
-  
+
     // // assigns the above to a variable
     // const tomorrowMorning = date.getTime();
-  
+
     // // logic to begin to filter items to today page
     // if (now > thisMorning && now < tomorrowMorning) {
     // }
@@ -77,9 +79,9 @@ export default function Today() {
               <h3>
                 {meal.Name}
                 <Link to={`/items/${meal._id}`}>
-                <button>Edit</button>
+                  <HiIcons.HiPencil />
                 </Link>
-                <button onClick={() => handleDelete(meal._id)}>Delete</button>
+                <FaIcons.FaTrash onClick={() => handleDelete(meal._id)} />
                 <br></br>
                 <h5>calories: {meal.Calories}</h5>
               </h3>
