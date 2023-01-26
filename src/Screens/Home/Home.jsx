310a18e2ffde2570ca113dcbddced8bad7c5598f
sdmantha <sdangelcake@gmail.com>
2023-01-25 20:39:47 -0500

    uploading change with an unseen character error

---
+++ src/Screens/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+import "../Home/Home.css";
+import React from "react";
+import NavBar from "../../Components/NavBar";
+
+function Home() {
+
+  return (
+    <div className="Home">
+    <NavBar />
+     <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
+
---
+++ src/Screens/Today/Today.jsx
@@ -0,0 +1,40 @@
+import NavBar from "../../Components/NavBar.jsx";
+import InputModal from "../../Components/InputModal.jsx";
+
+
+import { useState, useEffect } from "react";
+
+export default function Today() {
+  const [toggle, setToggle] = useState(false);
+  const [todaysMeals, setTodaysMeals] = useState([]);
+
+  const today = new Date();
+  console.log(today);
+
+  const display = () => {
+    if (!toggle) return setToggle(true);
+    return setToggle(false);
+  };
+
+  function apiCall() {
+    fetch();
+  }
+
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <NavBar />
+
+      <h1>Today</h1>
+
+      <button onClick={() => display()}>New Log Entry</button>
+
+      {toggle && <InputModal display={display} />}
+
+      <div className="todaysEntry">
+        <h3>{todaysMeals}</h3>
+      </div>
+    </div>
+  );
+}
