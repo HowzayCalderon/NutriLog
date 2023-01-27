6a31599ce3f8f497c9e7707535e93750f3f8c4b5
hairyElephant <josecalderon353@gmail.com>
2023-01-27 10:40:30 -0500

    done

--- src/Screens/History/History.jsx
+++ src/Screens/History/History.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { useEffect, useState } from "react";
 import NavBar from "../../Components/NavBar/NavBar.jsx";
@@ -18,7 +17,7 @@ function History() {
 
   return (
     <div>
-      <NavBar />
+      <NavBar History={"History"} />
       <p>{day[0].Name}</p>
     </div>
   );
